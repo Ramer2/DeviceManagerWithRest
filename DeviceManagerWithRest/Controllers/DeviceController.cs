9dba8e751ffe51a9a18b64e9b7b9995b0c56d4dc
s30422 <s30422@pjwstk.edu.pl>
2025-04-08 21:51:48 +0200

    Get methods described

--- DeviceManagerWithRest/Controllers/DeviceController.cs
+++ DeviceManagerWithRest/Controllers/DeviceController.cs
@@ -8,6 +8,20 @@ namespace DeviceManagerWithRest.Controllers;
 public class DeviceController : ControllerBase
 {
     private static readonly List<Device> _devices = new();
-    
-    
+
+    [HttpGet]
+    [Route("/devices")]
+    public IResult GetAllDevices()
+    {
+        var summaries = _devices.Select(d => new { d._id, d._name });
+        return Results.Ok(summaries);
+    }
+
+    [HttpGet]
+    [Route("/devices/{id}")]
+    public IResult GetDeviceById(string id)
+    {
+        var device = _devices.FirstOrDefault(d => d._id == id);
+        return Results.Ok(device);
+    }
 }
