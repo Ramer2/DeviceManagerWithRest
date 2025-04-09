12844d1a90e9420d635f0ed8e2b96b9781ada715
s30422 <s30422@pjwstk.edu.pl>
2025-04-09 16:22:34 +0200

    Added methods for creation, updating and removing already stored devices

--- DeviceManagerWithRest/Controllers/DeviceController.cs
+++ DeviceManagerWithRest/Controllers/DeviceController.cs
@@ -1,4 +1,6 @@
-﻿using Devices.devices;
+﻿using System.Text.Json;
+using Devices.devices;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeviceManagerWithRest.Controllers;
@@ -22,6 +24,55 @@ public class DeviceController : ControllerBase
     public IResult GetDeviceById(string id)
     {
         var device = _devices.FirstOrDefault(d => d._id == id);
+        return device == null ? Results.NotFound() : Results.Json(device);
+    }
+
+    [HttpPost]
+    [Route("/devices")]
+    public IResult AddDevice([FromBody] Device device)
+    {
+        // check already taken id
+        if (_devices.Any(d => d._id == device._id))
+        {
+            return Results.BadRequest("Device with this id already exists");
+        }
+        
+        _devices.Add(device);
+        return Results.Ok();
+    }
+
+    [HttpPut]
+    [Route("/devices/{id}")]
+    public IResult UpdateDevice(string id, [FromBody] Device updatedDevice)
+    {
+        // check indexes
+        if (id != updatedDevice._id)
+        {
+            return Results.BadRequest("Id's in passed device and in the URL must match");
+        }
+        
+        var index = _devices.FindIndex(d => d._id == id);
+        if (index == -1)
+        {
+            return Results.NotFound("Device with this id doesn't exist");
+        }
+
+        _devices[index] = updatedDevice;
+
+        return Results.Ok(updatedDevice);
+    }
+
+    [HttpDelete]
+    [Route("/devices/{id}")]
+    public IResult DeleteDevice(string id)
+    {
+        var device = _devices.FirstOrDefault(d => d._id == id);
+        if (device != null) _devices.Remove(device);
+        else
+        {
+            return Results.NotFound("Device with this id doesn't exist");
+        }
+        
         return Results.Ok(device);
     }
 }
